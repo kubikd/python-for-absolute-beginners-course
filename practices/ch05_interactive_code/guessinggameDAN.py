8c4c5f8e7bacbb6f43518c1d7f5991b58d1dbf60
kubikd <thedankubik@gmail.com>
2020-07-29 22:40:12 -0600

    Test Commit

---
+++ practices/ch05_interactive_code/guessinggameDAN.py
@@ -0,0 +1,9 @@
+import random
+
+print("--------------------------------------------------")
+print("              M&M Guessing Game!")
+print("--------------------------------------------------")
+
+mm_count = random.randint(1, 100)
+
+print(mm_count)
