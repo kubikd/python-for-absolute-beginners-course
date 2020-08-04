499e9fc39ffb351361291fee2533e362ef40b706
kubikd <thedankubik@gmail.com>
2020-08-04 14:40:03 -0600

    Through Chap5 - Exit early on a win

--- practices/ch05_interactive_code/guessinggameDAN.py
+++ practices/ch05_interactive_code/guessinggameDAN.py
@@ -3,7 +3,22 @@ import random
 print("--------------------------------------------------")
 print("              M&M Guessing Game!")
 print("--------------------------------------------------")
+# print(mm_count)
+
+print("Guess the number of M&Ms and you get lunch on the house!")
+print()
 
 mm_count = random.randint(1, 100)
+attempt_limit = 5
+attempts = 0
 
-print(mm_count)
+while attempts < attempt_limit:
+    guess_text = input("How many M&Ms are in the jar? ")
+    guess = int(guess_text)
+
+    if mm_count == guess:
+        print(f"You get a free lunch! It was {guess}.")
+    elif guess < mm_count        
+    attempts += 1
+
+
