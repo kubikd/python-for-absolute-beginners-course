f2d5ebb824ae34292363e4f0fa0cde1f7c618cb5
kubikd <thedankubik@gmail.com>
2020-08-04 22:16:29 -0600

    Final Version

--- practices/ch05_interactive_code/EvenOddDAN.py
+++ practices/ch05_interactive_code/EvenOddDAN.py
@@ -2,13 +2,22 @@ print("----------------------------------------------")
 print("         Even/Odd Number Identifier")
 print("----------------------------------------------")
 
-user_num = input("Enter any whole number: ")
-user_int = int(user_num)
+play_again = 'Y'
 
-if user_int % 2 == 0:
-    print("The number entered is EVEN!")
-    #break
-else:
-    print("The number entered is ODD!")
+while play_again == 'Y':
+    user_num = input("Enter any whole number: ")
+    user_int = int(user_num)
+    if user_int % 2 == 0:
+        print("The number entered is EVEN!")
+        print()
+        print()
+    else:
+        print("The number entered is ODD!")
+        print()
+        print()
+    play_again = input("Do you want to try again? (Y/N)")
 
-
+    if play_again == 'N':
+        print("Thank you for playing!")
+        print()
+        print()
