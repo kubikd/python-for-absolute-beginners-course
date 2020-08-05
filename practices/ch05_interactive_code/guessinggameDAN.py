8db4f57566090bdc3a3575f39db3663b94e31b55
kubikd <thedankubik@gmail.com>
2020-08-04 21:49:50 -0600

    Exercise 1

--- practices/ch05_interactive_code/guessinggameDAN.py
+++ practices/ch05_interactive_code/guessinggameDAN.py
@@ -15,10 +15,17 @@ attempts = 0
 while attempts < attempt_limit:
     guess_text = input("How many M&Ms are in the jar? ")
     guess = int(guess_text)
+    attempts += 1
 
     if mm_count == guess:
         print(f"You get a free lunch! It was {guess}.")
-    elif guess < mm_count        
-    attempts += 1
+        break
+    elif guess < mm_count:
+        print("Sorry, that's too LOW!")
+    else:
+        print("That's too HIGH!")
 
+
+print(f"The number of M&Ms is {mm_count}")
+print(f"You guessed the number of M&Ms in {attempts} tries!")
 
