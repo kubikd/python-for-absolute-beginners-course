1bb7ead08a7bab0c130e029fc4287583797e7507
kubikd <thedankubik@gmail.com>
2020-08-04 22:46:31 -0600

    Commit

--- practices/ch05_interactive_code/EvenOddZero.py
+++ practices/ch05_interactive_code/EvenOddZero.py
@@ -1,18 +1,21 @@
 print("----------------------------------------------")
 print("         Even/Odd/Zero Identifier")
 print("----------------------------------------------")
-    
-user_num = input("Enter any whole number or enter Zero to Stop Playing: ")
-user_int = int(user_num)
-if user_int == 0:
-    print("You entered Zero.  Thank you for playing!")
-    print()
-    print()
-elif user_int % 2 == 0:
-    print("The number entered is EVEN!")
-    print()
-    print()
-else:
-    print("The number entered is ODD!")
-    print()
-    print()
+
+user_num = 1
+
+while user_int != 0:
+    user_num = input("Enter any whole number or enter Zero to Stop Playing: ")
+    user_int = int(user_num)
+    if user_int == 0:
+        print("You entered Zero.  Thank you for playing!")
+        print()
+        print()
+    elif user_int % 2 == 0:
+        print("The number entered is EVEN!")
+        print()
+        print()
+    else:
+        print("The number entered is ODD!")
+        print()
+        print()
