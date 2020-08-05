107544c81d023bc293b6cbe855ff081f1a5b5146
kubikd <thedankubik@gmail.com>
2020-08-04 22:14:31 -0600

    Final Version

---
+++ practices/ch05_interactive_code/EvenOddDAN.py
@@ -0,0 +1,14 @@
+print("----------------------------------------------")
+print("         Even/Odd Number Identifier")
+print("----------------------------------------------")
+
+user_num = input("Enter any whole number: ")
+user_int = int(user_num)
+
+if user_int % 2 == 0:
+    print("The number entered is EVEN!")
+    #break
+else:
+    print("The number entered is ODD!")
+
+
